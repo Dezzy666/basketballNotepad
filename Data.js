760d1edf136cc8b4df4f911d252160c528d86b78
Jan Herzan <jan.herzan@gmail.com>
2014-04-23 19:05:34 +0200

    Data creation edited. Method getDataOfPlayer added.

--- Data.js
+++ Data.js
@@ -4,23 +4,64 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
+/**
+* Data creation constructor
+*
+* @method Data
+* @author Jan Herzan
+*/
 function Data() {
-    this.players = new Array();
-    this.players.push({ playerNumber: 2, name: "Name2" });
-    this.players.push({ playerNumber: 3, name: "Name1" });
-    this.players.push({ playerNumber: 6, name: "Name3" });
-    this.players.push({ playerNumber: 8, name: "Name4" });
-    this.players.push({ playerNumber: 10, name: "Name5" });
-    this.players.push({ playerNumber: 12, name: "Name6" });
-    this.players.push({ playerNumber: 17, name: "Name7" });
-    this.players.push({ playerNumber: 18, name: "Name8" });
-    this.players.push({ playerNumber: 19, name: "Name9" });
-    this.players.push({ playerNumber: 20, name: "Name10" });
-    this.players.push({ playerNumber: 21, name: "Name11" });
-    this.players.push({ playerNumber: 22, name: "Name12" });
-    this.players.push({ playerNumber: 24, name: "Name13" });
+    this.players = [];
+    this.players.push(this.createPlayer(4, "Name4"));
+    this.players.push(this.createPlayer(5, "Name5"));
+    this.players.push(this.createPlayer(6, "Name6"));
+    this.players.push(this.createPlayer(7, "Name7"));
+    this.players.push(this.createPlayer(8, "Name8"));
+    this.players.push(this.createPlayer(9, "Name9"));
+    this.players.push(this.createPlayer(10, "Name10"));
+    this.players.push(this.createPlayer(11, "Name11"));
+    this.players.push(this.createPlayer(12, "Name12"));
+    this.players.push(this.createPlayer(13, "Name13"));
+    this.players.push(this.createPlayer(14, "Name14"));
+    this.players.push(this.createPlayer(15, "Name15"));
 }
 
+/**
+* Creates data for one player
+*
+* @method createPlayer
+* @author Jan Herzan
+* @param Players number
+* @param Players name
+*/
+Data.prototype.createPlayer = function (playerNumber, playerName) {
+    return { playerNumber: playerNumber, name: playerName, faulPlus: 1, faulMinus: 2, gain: 2, loss: 3, reboundO: 3, reboundD: 2, penaltsGetted: 5, penaltsScored: 2, shots: [{ x: 100, y: 130, scored: true }, { x: 100, y: 130, scored: true }], shotsUnderBasket: 2, shotsUnderBasketScored: 2 };
+}
+
+/**
+* Returns data of players
+*
+* @method getData
+* @author Jan Herzan
+*/
 Data.prototype.getData = function () {
     return this.players;
+}
+
+/**
+* Returns data of one player
+*
+* @method getDataOfPlayer
+* @author Jan Herzan
+* @param {Integer} Players number
+* @return {Object} Players data
+*/
+Data.prototype.getDataOfPlayer = function (playerNumber) {
+    for (var i = 0; i < this.players.length; i++) {
+        if (this.players[i].playerNumber === playerNumber) {
+            return this.players[i];
+        }
+    }
+
+    return undefined;
 }
