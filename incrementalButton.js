72eea5bcac7231a1cbf13ae7e600b21c20337f08
Jan Herzan <jan.herzan@gmail.com>
2014-04-24 17:13:41 +0200

    Incremental button added.

---
+++ incrementalButton.js
@@ -0,0 +1,65 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="incrementalButton.js" company="Perfect Long Bow s.r.o.">
+//     Copyright Perfect Long Bow s.r.o.
+// </copyright>
+//-----------------------------------------------------------------------
+
+$.widget("basketballNotepad.incrementalButton", {
+    options: {
+        // value of button
+        value: 0,
+        // bounds
+        minBound: 0,
+        maxBound: 5,
+        // if true buttons value is incremented on click
+        incrementalByTap: true,
+        // when value changes - callback
+        valueChanged: function (value, element) { },
+        // on click event
+        onClickEvent: function (e) { e.preventDefault(); }
+    },
+
+    // Creates new button
+    _create: function () {
+        this.setValue(this.options.value);
+        this.element.addClass("incrementalButtonMain");
+
+        if (this.options.incrementalByTap) {
+            this.element.on('click', (function () {
+                if (this.options.maxBound === -1 || this.options.value < this.options.maxBound) {
+                    this.options.value = this.options.value + 1;
+                    this.element.html(this.options.value);
+                    this.options.valueChanged(this.options.value, this.element);
+                }
+            }).bind(this));
+
+            this.element.on('taphold', (function (e) {
+                e.preventDefault();
+                if (this.options.value > this.options.minBound) {
+                    this.options.value = this.options.value - 1;
+                    this.element.html(this.options.value);
+                    this.options.valueChanged(this.options.value, this.element);
+                }
+            }).bind(this));
+
+            this.element.on('contextmenu', (function (e) {
+                e.preventDefault();
+                if (this.options.value > this.options.minBound) {
+                    this.options.value = this.options.value - 1;
+                    this.element.html(this.options.value);
+                    this.options.valueChanged(this.options.value, this.element);
+                }
+            }).bind(this));
+        } else {
+            this.element.on('click', this.options.onClickEvent);
+        }
+    },
+
+    // sets the value
+    setValue: function (value) {
+        this.options.value = value;
+        this.element.html(this.options.value);
+        this.options.valueChanged(this.options.value, this.element);
+    }
+
+});
